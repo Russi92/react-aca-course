ac6b14e48d6becb3c96a2344dc679bb261864900
Mohamed RUالإيميل المسجل في GitHub! <mr.rossy2020@gmail.com>
2025-05-09 22:26:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+
+
 import logo from './logo.svg';
 import './App.css';
+import Russi from './Russi'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      < Russi />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Russi />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,4 +27,7 @@ function App() {
   );
 }
 
+
+
 export default App;
+
---
+++ src/Russi.js
@@ -0,0 +1,40 @@
+
+
+export default function Russi(){
+    const x = 100;
+
+    const person={
+        name : 'Mohamed',
+        email : 'mohamed@gmail.com'
+    }
+
+    const elmStyle={
+      background : 'green',
+      color : 'black',
+      boxShadow : '0 20px 10px red'
+    }
+
+    return(
+      <div>
+        <h1 style={elmStyle}>{sayHello()} hello im russi {x - 90}</h1>
+        <h5 style={{fontSize:'50px'}}>{person.name}</h5>
+        <h5>{person.email}</h5>
+        <div style={elmStyle}>nedved</div>
+        <button onClick={clickBtn}>Log In</button>
+      </div>
+  
+    )
+  }
+
+  function sayHello(){
+    return('hey >>>')
+  }
+
+function clickBtn(){
+  alert('i love gogo')
+}
+
+
+
+
+//   export default Russi;
